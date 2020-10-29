1d2ccb9f87e3d1652bff401fb1230ea2fd081b0b
marianograziano <mariano.graziano@gmail.com>
2020-10-29 16:59:18 -0300

    Ejercicios 1 - 3 inclusive.

--- src/front/ts/main.ts
+++ src/front/ts/main.ts
@@ -5,6 +5,91 @@
  * Project: DAW - CEIoT - Project Structure
  * Brief: Main frontend file (where the logic is)
 =============================================================================*/
+class User {
+    private _id:number;
+    private _name:string;
+    private _email:string;
+    private _isLogged:boolean;
+    
+    constructor (id:number, name:string, email:string) {
+        this._id = id;
+        this._name = name;
+        this._email = email;
+    }
+
+    set id(id:number){
+        this._id = id
+    }
+    
+     get id():number{
+         return this._id
+     }
+
+    set name(id:string){
+        this._name = name;
+    }
+    
+    get name():string{
+         return this._name
+     }
+
+    set email(email:string){
+        this._email = email;
+    }
+    
+    get email():string{
+         return this._email
+     }
+    
+    set islogged(islogged:boolean){
+        this.islogged = islogged;
+    }
+    
+    get islogged():boolean{
+         return this.islogged
+     }
+
+    printInfo():void{
+        console.log("id : " + this._id + ") nombre : " + this._name + " email : " + this.email)
+
+
+    }
+
+
+}
+
+
+
+
+
+class Main {
+    main():void {
+        console.log("Mensaje desde Main");
+        
+        let usuarios:Array<User>;
+        usuarios = new Array<User>();
+        usuarios.push(new User(1,"Mariano","mariano@gmail.com"))
+        usuarios.push(new User(2,"Juan","juan@gmail.com"))
+        usuarios.push(new User(3,"Pedro","pedro@gmail.com"))
+
+        for (let i in usuarios)
+        {
+            usuarios[i].printInfo()
+        }
+
+
+
+}
+}
+
+
+
+window.onload = function() {
+        let m:Main = new Main()
+        m.main ();
+
+}
+
 
 //=======[ Settings, Imports & Data ]==========================================
 
@@ -15,7 +100,8 @@ let user = "TypesScript Users!";
 function greeter(person) {
     return "Hello, " + person;
  }
- 
- document.body.innerHTML = greeter(user);
+
+ //document.body.innerHTML = greeter(user);
+ console.log("Hola mundo!");
 
 //=======[ End of file ]=======================================================
